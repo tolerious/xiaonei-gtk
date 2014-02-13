a4294a8bccaa5c09976ff2115275528ba68e29c9
fengxiaolong <aldrichnjit@gmail.com>
2014-02-13 21:53:00 +0800

    complete Makefile

---
+++ GetAccessToken.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   GetAccessToken.h
+ * Author: fengtianba
+ *
+ * Created on February 13, 2014, 9:48 PM
+ */
+
+#ifndef GETACCESSTOKEN_H
+#define	GETACCESSTOKEN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GETACCESSTOKEN_H */
+
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/GetAccessToken.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/xiaonei-gtk: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/xiaonei-gtk ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/GetAccessToken.o: GetAccessToken.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/GetAccessToken.o GetAccessToken.c
 
 ${OBJECTDIR}/main.o: main.c 
 	${MKDIR} -p ${OBJECTDIR}
