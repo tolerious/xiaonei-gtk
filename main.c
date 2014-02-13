9c8a5e098fcbaf3fd5334ee5a472ceb1ba1d7431
fengxiaolong <aldrichnjit@gmail.com>
2014-02-13 21:46:19 +0800

    init project

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: fengtianba
+ *
+ * Created on February 13, 2014, 9:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
