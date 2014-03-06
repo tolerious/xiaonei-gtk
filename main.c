584e57d3085c87b33447407d1bed1a98d9abbe99
fengxiaolong <aldrichnjit@gmail.com>
2014-03-06 15:47:46 +0800

    add decode

--- main.c
+++ main.c
@@ -9,13 +9,23 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include "AccessToken.h"
+#include <curl/curl.h>
 /*
  * 
  */
 int main(int argc, char** argv) {
     GetAccessToken();
+    int i = 100;
+    char *decode_string;
     system("firefox result.html");
     puts("\n\nget AccessToken successful\n\n");
+    CURL *curl;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    decode_string = curl_easy_unescape(curl, ACCESS_TOKEN, 0, &i);
+    printf("origin url: %s\n", ACCESS_TOKEN);
+    printf("new    url: %s\n", decode_string);
+    curl_easy_cleanup(curl);
     
     
     
