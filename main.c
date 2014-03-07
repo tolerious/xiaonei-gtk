f909885550653b6dbedccafbda50fe6a0261bff9
fengxiaolong <aldrichnjit@gmail.com>
2014-03-07 12:09:23 +0800

    show window

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <gtk/gtk.h>
 #include "AccessToken.h"
 #include <curl/curl.h>
+#include <glib-2.0/glib/gerror.h>
 /*
  * 
  */
@@ -32,21 +33,51 @@ int main(int argc, char** argv) {
     xiaonei_gtk_get_blog_list(accesstoken);
     //xiaonei_gtk_create_one_blog(accesstoken);
     
+    //gtk wdiget init start...
+    GtkWidget *window;
+    GtkBuilder *builder;
+    GError *error = NULL;
+    
+    //widget init
+    gtk_init(&argc, &argv);
+    builder = gtk_builder_new();
+    if(!gtk_builder_add_from_file(builder, "./sources/glade.ui", &error))
+    {
+        g_print("Error occured while loading UI files\n");
+        g_print("Message: %s\n", error->message);
+        g_free(error);
+        return 1;
+    }
+    
+    //widget get from builder(glade files)
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    
+    
+    
+    //set object attributes
+    gtk_window_set_title(GTK_WINDOW(window), "Xiao nei Gtk Demo");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    
+    
+    
+    
+    
+    //signal to connect to widget
+    g_signal_connect(window, "delete-event", gtk_main_quit, NULL);
     
     
     
     
     
     
+    //object unref
+    g_object_unref(G_OBJECT(builder));
     
     
-    
-    
-    
-    
-    
-    
-    
+    //show widget 
+    gtk_widget_show_all(window);
+    gtk_main();
     
     return (EXIT_SUCCESS);
 }
